33b3ec40d682901685e347995f5662f652eb6b37
Sangjoong Kim <sangjoong.kim@fitogether.com>
2020-01-14 09:08:46 +0900

    함수명 변경 및 15일 문제 준비

--- problems/level1/2020-01-14/solution.js
+++ problems/level1/2020-01-14/solution.js
@@ -1,12 +1,12 @@
-const solve = arr => {
-  let answer = arr.filter((a, i) => a !== arr[i + 1]);
-  console.log(answer);
-};
-
 //TEST CASE1
 const arr = [1, 1, 3, 3, 0, 1, 1];
-
 //TEST CASE2
 // const arr = [4, 4, 4, 3, 3];
 
-solve(arr);
+const solution = arr => {
+  let answer = arr.filter((a, i) => a !== arr[i + 1]);
+  // console.log(answer);
+  return answer;
+};
+
+console.log(solution(arr));
