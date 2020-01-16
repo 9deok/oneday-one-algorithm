16a7283f8cbe001ee1f1193ca8ec4ba9a86622a7
Sangjoong Kim <sangjoong.kim@fitogether.com>
2020-01-16 09:04:21 +0900

    2020-01-16

---
+++ problems/level1/2020-01-16/solution.js
@@ -0,0 +1,35 @@
+/*TEST CASE1 */
+// const a = 3;
+// const b = 5;
+
+/*TEST CASE2 */
+// const a = 3;
+// const b = 3;
+
+/*TEST CASE3 */
+const a = 5;
+const b = 3;
+
+const solution = (a, b) => {
+  let min = a < b ? a : b;
+  let max = a > b ? a : b;
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+console.log(solution(a, b));
+
+/**
+ * 프로그래머스 다른사람 문제 풀이
+ */
+
+const otherSolution = (a, b) => {
+  let result = 0;
+  return ((a + b) * (Math.abs(b - a) + 1)) / 2;
+};
+
+console.log(otherSolution(a, b));
