b067af9a6a7ff8cdb0f1f337cdbb906ad1d7e981
김상중 <qqqq3115@naver.com>
2020-02-11 23:41:54 +0900

    2020-02-11 풀이 이해시간 부족

---
+++ problems/level2/2020-02-11/solution.js
@@ -0,0 +1,22 @@
+/*TEST CASE1 */
+// const bridge_length = 2;
+// const weight = 10;
+// const truck_weight = [7,4,5,6];
+
+/*TEST CASE1 */
+// const bridge_length = 100;
+// const weight = 100;
+// const truck_weight = [10];
+
+/*TEST CASE1 */
+const bridge_length = 100;
+const weight = 100;
+const truck_weight = [10,10,10,10,10,10,10,10,10,10];
+
+const solution = (bridge_length,weight,truck_weight) => {
+  const truckCount = truck_weight.length;
+
+};
+  
+console.log(solution(bridge_length,weight,truck_weight));
+  
