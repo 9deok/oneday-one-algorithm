dd255e14542e5046aff01629e36ca445353e80fa
9deok <qqqq3115@naver.com>
2020-02-19 21:04:04 +0900

    2월 19일 알고리즘 풀이 - 미해결

---
+++ problems/level2/2020-02-19/solution.js
@@ -0,0 +1,45 @@
+/*TEST CASE1 */
+// const number = "1924";
+// const k = 2;
+
+/*TEST CASE2 */
+// const number = "1231234";
+// const k = 3;
+
+/*TEST CASE3 */
+const number = "4177252841";
+const k = 4;
+
+const solution = (number, k) => {
+  // let test = number.split("");
+  // let test3 = [];
+  // console.log(test);
+  // test2 = test.map((a, i, arr) => {
+  //   arr.map((b, j) => {
+  //     if (i > j) {
+  //       console.log(`b ==> ${b}  arr[i] ===> ${arr[i]}`);
+  //       test3.push(b + arr[i]);
+  //     }
+  //   });
+  // });
+  // return test3.sort((a, b) => b - a)[0];
+
+  let numArr = number.split("");
+  let fullArr = allArr(numArr);
+};
+
+const allArr = arr => {
+  let result = [];
+  arr.map();
+};
+
+// console.log(solution(number, k));
+
+const otherSolution = (number, k) => {
+  let b = [];
+  for (let i = 0; i < number.length; i++) {
+    let c = number[i];
+  }
+};
+
+console.log(otherSolution(number, k));
