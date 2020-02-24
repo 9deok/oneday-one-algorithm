857c9981e6ece1cdb1c763349deb1871da724e97
9deok <qqqq3115@naver.com>
2020-02-24 10:16:59 +0900

    level2 탑 재도전
    성공

--- problems/level2/2020-02-07/retry.js
+++ problems/level2/2020-02-07/retry.js
@@ -8,7 +8,32 @@ const heights = [6, 9, 5, 7, 4];
 // const heights = [1, 5, 3, 6, 7, 6, 5];
 
 const solution = heights => {
-  return "test";
+  let answer = [];
+  answer[0] = 0;
+  heights.map((a, i, arr) => {
+    for (let j = arr.length - i - 1; j > 0; j--) {
+      let index = Math.abs(i - arr.length + 1);
+      if (arr[index] < arr[j - 1]) {
+        answer[index] = j;
+        break;
+      } else {
+        answer[index] = 0;
+      }
+    }
+  });
+  return answer;
 };
 
-console.log(solution(heights));
+const otherSolution = heights => {
+  let answer = heights.map((a, i) => {
+    while (j) {
+      j--;
+      if (heights[j] > a) return i + 1;
+    }
+    return 0;
+  });
+  return answer;
+};
+
+// console.log(solution(heights));
+console.log(otherSolution(heights));
